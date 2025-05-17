2f280abcec7b11ba30345f86cd844399619b568a
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-05-16 21:37:47 -0300

    Initial Commit

---
+++ SwiftTestingBasics/SwiftTestingBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingBasicsApp.swift
+//  SwiftTestingBasics
+//
+//  Created by Gabriel on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTestingBasicsTests/SwiftTestingBasicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingBasicsTests.swift
+//  SwiftTestingBasicsTests
+//
+//  Created by Gabriel on 16/05/25.
+//
+
+import Testing
+@testable import SwiftTestingBasics
+
+struct SwiftTestingBasicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
